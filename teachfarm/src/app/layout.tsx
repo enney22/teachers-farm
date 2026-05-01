ccedaaefa28c666a79514386c47d15e779f6d899
Zayzay J. Yennego <zyennego@gmail.com>
2026-05-01 11:46:21 +0000

    feat: integrate Google Analytics GA4

--- teachfarm/src/app/layout.tsx
+++ teachfarm/src/app/layout.tsx
@@ -119,6 +119,20 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        
+        {/* Google Analytics GA4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KLX1XTXXZM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KLX1XTXXZM');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
